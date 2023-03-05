d99798d43931712c8c4f0b46dc3cb11485e51582
Sven Skender <sskender@pm.me>
2023-03-05 22:54:23 +0100

    Implement Cardano network info route

--- src/middleware/cardano/node-info.js
+++ src/middleware/cardano/node-info.js
@@ -1,9 +1,11 @@
 const httpStatus = require('http-status');
+const cardano = require('../../utils/cardano');
 
 function getNodeInfo(req, res, next) {
+  const queryTip = cardano.queryTip();
   return res.status(httpStatus.OK).json({
     success: true,
-    data: null,
+    data: queryTip,
     status: httpStatus.OK,
   });
 }
