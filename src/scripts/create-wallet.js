92ac3cfa73c4028b07b4323b5ba7b3cd88c80bd6
Sven Skender <sskender@pm.me>
2023-03-18 18:54:59 +0100

    Create wallets from command line

--- src/scripts/create-wallet.js
+++ src/scripts/create-wallet.js
@@ -1,5 +1,16 @@
-const config = require('../config');
+const logger = require('../utils/logger');
 const createNewWallet = require('../services/cardano-wallet');
 
-const walletName = config.cardano.walletName;
+const argv = process.argv;
+
+if (argv.length <= 2) {
+  throw new Error('Missing wallet name!');
+}
+
+const walletName = argv[2];
+
+logger.info(`Creating wallet '${walletName}' ...`);
+
 createNewWallet(walletName);
+
+logger.info(`Created wallet '${walletName}' successfully!`);
